f9ab7a466faefa90d362eaae3d52cfcccb23b19a
Aizaz Haider <aizaz265@gmail.com>
2024-02-19 22:59:36 +0500

    first commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e031df3197mshcc70b9c48033da4p1c785bjsnccb6ccff5f55',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "9cf81cea5a37075ec3d52480f92311c3";
+ 
+
